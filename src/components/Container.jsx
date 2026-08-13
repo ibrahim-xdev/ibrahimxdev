d283507439fa2c21e37b779c07b549c7c7528702
Muhammad Ibrahim <mi6054342@gmail.com>
2026-08-13 18:39:25 +0500

    Update portfolio

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,5 +1,7 @@
 export default function Container({ children, className = "" }) {
   return (
-    <div className={`mx-auto w-full max-w-6xl ${className}`}>{children}</div>
+    <div className={`mx-auto w-full max-w-6xl px-6 lg:px-0 ${className}`}>
+      {children}
+    </div>
   );
 }
