be0c9dc04ffef6136d3ed47bbcc10e37754cf386
Muhammad Ibrahim <mi6054342@gmail.com>
2026-08-21 15:59:04 +0500

    added repo name

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/ibrahimxdev/",
   plugins: [react(), tailwindcss()],
   base: "/ibrahimxdev/",
 });
